50ed94224fb730735a0dd22f56207d7a293c7f18
Trace Wimber <trace.wimber@gmail.com>
2024-07-03 16:32:29 -0600

    Move and PositionTests Complete

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -7,6 +7,14 @@ public class KingMovesCalculator implements MoveCalculator {
 
     @Override
     public Set<ChessMove> calcMoves(ChessBoard board, ChessPosition myPosition) {
-        return new HashSet<>();
+        Set<ChessMove> moves = new HashSet<>();
+        int row = myPosition.getRow() - 1;      // Convert to 0-based index
+        int col = myPosition.getColumn() - 1;   // Convert to 0-based index
+
+        // Directions the King can move
+        int[] rowDirs = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] colDirs = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        return moves;
     }
 }
