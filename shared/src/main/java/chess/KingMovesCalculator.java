30030529dc35342e93587c98927d0ba6436863fe
Trace Wimber <trace.wimber@gmail.com>
2024-07-03 16:47:24 -0600

    KingMoveTests complete

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -15,6 +15,27 @@ public class KingMovesCalculator implements MoveCalculator {
         int[] rowDirs = {-1, -1, -1, 0, 0, 1, 1, 1};
         int[] colDirs = {-1, 0, 1, -1, 1, -1, 0, 1};
 
+        // Check in each direction
+        for (int i = 0; i < 8; i++) {
+            int currRow = row + rowDirs[i];
+            int currCol = col + colDirs[i];
+
+            // Add only if the move is on the board
+            if (isValid(currRow, currCol)) {
+                ChessPosition currPos = new ChessPosition(currRow + 1, currCol + 1);
+                ChessPiece pieceAtPos = board.getPiece(currPos);
+                // Add only if position is open or contains enemy
+                if (pieceAtPos == null || !pieceAtPos.getTeamColor().equals(board.getPiece(myPosition).getTeamColor())) {
+                    moves.add(new ChessMove(myPosition, new ChessPosition(currRow + 1, currCol + 1), null));
+                }
+            }
+        }
+
         return moves;
+    }
+
+    // Check if a position is on the board (uses 0-based indexing)
+    private boolean isValid(int row, int col) {
+        return row >= 0 && row < 8 && col >= 0 && col < 8;
     }
 }
