3b144f60da0f471b694f87489166fdd2e2dff8dc
Trace Wimber <trace.wimber@gmail.com>
2024-07-31 21:11:10 -0600

    All AuthDAO Unit Tests Complete

--- server/src/test/java/dataaccess/SqlDAOTests.java
+++ server/src/test/java/dataaccess/SqlDAOTests.java
@@ -112,9 +112,59 @@ public class SqlDAOTests {
     }
 
     //------------DELETE AUTH positive & negative tests---------------
+    @Test
+    @DisplayName("Delete Auth Works")
+    public void deleteAuth() throws Exception {
+        try (var conn = DatabaseManager.getConnection()) {
+            var pdStmt = conn.prepareStatement("INSERT INTO auth (authToken, username) VALUES (?, ?)");
+            pdStmt.setString(1, userAuth1.authToken());
+            pdStmt.setString(2, userAuth1.username());
+            pdStmt.executeUpdate();
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+
+        authDAO.deleteAuth("1234");
+
+        try (var conn = DatabaseManager.getConnection()) {
+            var pdStmt = conn.prepareStatement("SELECT * FROM auth WHERE authToken = ?");
+            pdStmt.setString(1, "1234");
+            var resultSet = pdStmt.executeQuery();
+            Assertions.assertFalse(resultSet.isBeforeFirst());
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
+
+    @Test
+    @DisplayName("Auth Not Found")
+    public void cannotDeleteAuth() {
+        Executable delAuth = () -> authDAO.deleteAuth("1234");
+        Assertions.assertThrows(DataAccessException.class, delAuth);
+    }
 
     //------------CLEAR test------------------------------------------
+    @Test
+    @DisplayName("Clear Works")
+    public void clearAuth() throws Exception {
+        AuthData userAuth2 = new AuthData("5678", "player2");
+        AuthData userAuth3 = new AuthData("91011", "player3");
+        authDAO.createAuth(userAuth1);
+        authDAO.createAuth(userAuth2);
+        authDAO.createAuth(userAuth3);
+        authDAO.clear();
 
+        try (var conn = DatabaseManager.getConnection()) {
+            var pdStmt = conn.prepareStatement("SELECT * FROM auth");
+            var resultSet = pdStmt.executeQuery();
+            Assertions.assertFalse(resultSet.isBeforeFirst());
+        }
+        catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
+        }
+    }
 
     //=========================GameDAO=======================
 
